dd5e1339f8ba7a41ee0eb31ad690de4a4522b1d6
Jan Kaiser <jan.kaiser.email@googlemail.com>
2021-04-14 21:10:05 +0200

    Ran tests with new stepper board

--- stepper_test.py
+++ stepper_test.py
@@ -1,26 +1,47 @@
 import RPi.GPIO as GPIO
 from time import sleep
 
-DIRPIN, STEPPIN = 21, 20
+ENABLE_PIN = 16
+DIRECTION_PIN = 21
+STEP_PIN = 20
+
+STEPS_PER_ROTATION = 200
+DELAY = 0.0006
+
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(DIRPIN, GPIO.OUT)
-GPIO.setup(STEPPIN, GPIO.OUT)
+GPIO.setup(ENABLE_PIN, GPIO.OUT, initial=GPIO.HIGH)
+GPIO.setup(DIRECTION_PIN, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(STEP_PIN, GPIO.OUT, initial=GPIO.LOW)
 
-def set_direction_left():
-    GPIO.output(DIRPIN, GPIO.LOW)
+print("Motor disabled")
+sleep(3)
 
-def set_direction_right():
-    GPIO.output(DIRPIN, GPIO.HIGH)
+# Enable stepper motor
+GPIO.output(ENABLE_PIN, GPIO.LOW)
+print("Motor enabled")
+sleep(3)
 
-def step(delay=0.005):
-    GPIO.output(STEPPIN, GPIO.HIGH)
-    sleep(delay)
-    GPIO.output(STEPPIN, GPIO.LOW)
-    sleep(delay)
+print("Motor running")
+# Set direction left
+GPIO.output(DIRECTION_PIN, GPIO.LOW)
 
-set_direction_left()
+# Do roations
+for i in range(5):
+    print(f"Iteration {i}")
+    for rotation in range(3):
+        for step in range(STEPS_PER_ROTATION):
+            GPIO.output(STEP_PIN, GPIO.HIGH)
+            sleep(DELAY)
+            GPIO.output(STEP_PIN, GPIO.LOW)
+            sleep(DELAY)
+    sleep(1)
 
-while(True):
-    step(delay=0.0025)
+print("Motor stopped and enabled")
+sleep(3)
 
-# GPIO.cleanup()
+# Disable stepper motor
+GPIO.output(ENABLE_PIN, GPIO.HIGH)
+print("Motor disabled")
+sleep(3)
+
+GPIO.cleanup()
