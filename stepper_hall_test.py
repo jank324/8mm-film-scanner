6bcc0cfcb2713626339171a7e2e3ec84e2f3ecdf
Jan Kaiser <jan.kaiser.email@googlemail.com>
2021-04-29 18:31:28 +0200

    Install hall effect sensor and magnet properly

--- stepper_hall_test.py
+++ stepper_hall_test.py
@@ -1,50 +1,50 @@
-import RPi.GPIO as GPIO
 from time import sleep
 
-ENABLE_PIN = 16
-DIRECTION_PIN = 21
-STEP_PIN = 20
-HALL_PIN = 26
+import RPi.GPIO as GPIO
 
-STEPS_PER_ROTATION = 200
-DELAY = 0.0008
+enpin = 16
+dirpin = 21
+steppin = 20
+hallpin = 26
+
+steps_per_rotation = 200
+delay = 0.0008
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(ENABLE_PIN, GPIO.OUT, initial=GPIO.HIGH)
-GPIO.setup(DIRECTION_PIN, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(STEP_PIN, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(HALL_PIN, GPIO.IN)
+GPIO.setup(enpin, GPIO.OUT, initial=GPIO.HIGH)
+GPIO.setup(dirpin, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(steppin, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(hallpin, GPIO.IN)
 
 print("Enable motor")
-GPIO.output(ENABLE_PIN, GPIO.LOW)
+GPIO.output(enpin, GPIO.LOW)
 
 print("Set motor direction counter-clockwise")
-GPIO.output(DIRECTION_PIN, GPIO.LOW)
+GPIO.output(dirpin, GPIO.LOW)
 
 print("Register hall effect detection")
-is_detected = False
-was_detected = False
+hall_effect_detected = False
 def hall_detected(channel):
-    global is_detected
-    global was_detected
-    global i
-    if not was_detected:
-        print("Hall effect detected -> stopping motor at", i)
-        is_detected = True
-        sleep(1)
-        is_detected = False
-        was_detected = True
-        i = 0
-GPIO.add_event_detect(HALL_PIN, GPIO.FALLING, callback=hall_detected)
+    global hall_effect_detected
+    hall_effect_detected = True
+GPIO.add_event_detect(hallpin, GPIO.RISING, callback=hall_detected)
 
 # Run motor unless interupted
 i = 0
+leaving = False
 while True:
-    if not is_detected:
-        GPIO.output(STEP_PIN, GPIO.HIGH)
-        sleep(DELAY)
-        GPIO.output(STEP_PIN, GPIO.LOW)
-        sleep(DELAY)
-        i += 1
-    if i > 100 and was_detected:
-        was_detected = False
+    if hall_effect_detected and not leaving:
+        print(f"Taking photo at {i}")
+        sleep(1)
+        i = 0
+        leaving = True
+    
+    if leaving and i > 200:
+        leaving = False
+        hall_effect_detected = False
+
+    GPIO.output(steppin, GPIO.HIGH)
+    sleep(delay)
+    GPIO.output(steppin, GPIO.LOW)
+    sleep(delay)
+    i += 1
