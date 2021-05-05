5afc950568f37db0d3901ff03da90be2d030fe4e
Jan Kaiser <jan.kaiser.email@googlemail.com>
2021-05-05 19:11:31 +0200

    Implemented class for hall effect sensor

--- run.py
+++ run.py
@@ -1,29 +1,20 @@
 import RPi.GPIO as GPIO
-from machine import Stepper
+from machine import StepperMotor, HallEffectSensor
 from time import sleep
 
 
 GPIO.setmode(GPIO.BCM)
 
-motor = Stepper(enable_pin=16, direction_pin=21, step_pin=20)
-
-hallpin=26
-GPIO.setup(hallpin, GPIO.IN)
+motor = StepperMotor(enable_pin=16, direction_pin=21, step_pin=20)
+sensor = HallEffectSensor(input_pin=26)
 
 motor.enable()
-
-print("Register hall effect detection")
-hall_effect_detected = False
-def hall_detected(channel):
-    global hall_effect_detected
-    hall_effect_detected = True
-GPIO.add_event_detect(hallpin, GPIO.RISING, callback=hall_detected)
 
 # Run motor unless interupted
 i = 0
 leaving = False
 while True:
-    if hall_effect_detected and not leaving:
+    if sensor.rising_edge_detected and not leaving:
         print(f"Taking photo at {i}")
         sleep(1)
         i = 0
@@ -31,7 +22,7 @@ while True:
     
     if leaving and i > 200:
         leaving = False
-        hall_effect_detected = False
+        sensor.rising_edge_detected = False
 
     motor.step()
     i += 1
