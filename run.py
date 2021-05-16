f40d5c3c0d5fc174fb79b54107c347c755702698
Jan Kaiser <jan.kaiser.email@googlemail.com>
2021-05-16 19:07:22 +0200

    Implmented class for the entire machine

--- run.py
+++ run.py
@@ -1,60 +1,14 @@
 import signal
-import sys
-from time import sleep
 
-import picamera
-import RPi.GPIO as GPIO
-
-from machine import Circuit, HallEffectSensor, Light, StepperMotor
+from machine import Machine
 
 
 close_requested = False
 
 def cleanup_and_close(sig, frame):
-    global close_requested
-    close_requested = True
+    machine.close_requested = True
 
 signal.signal(signal.SIGINT, cleanup_and_close)
 
-GPIO.setmode(GPIO.BCM)
-
-motor = StepperMotor(enable_pin=16, direction_pin=21, step_pin=20)
-sensor = HallEffectSensor(input_pin=26)
-light = Light()
-
-circuit12v = Circuit(switch_pin=6, loads=[light,motor])
-
-light.turn_on()
-sleep(1)
-motor.enable()
-
-# with picamera.PiCamera() as camera:
-if True:
-    # Run motor unless interupted
-    i = 0
-    photo_index = 249
-    leaving = True
-    while True:
-        if sensor.rising_edge_detected and not leaving:
-            motor.decelerate()
-            print(f"Taking photo {photo_index} at {i}")
-            # camera.capture(f"testrun/img{photo_index:05}.jpg")
-            sleep(0.7)
-            photo_index += 1
-            i = 0
-            leaving = True
-
-            if close_requested:
-                motor.disable()
-                light.turn_off()
-                GPIO.cleanup()
-                sys.exit(0)
-            else:
-                motor.accelerate()
-        
-        if leaving and i > 200:
-            leaving = False
-            sensor.rising_edge_detected = False
-
-        motor.step()
-        i += 1
+machine = Machine()
+machine.run()
