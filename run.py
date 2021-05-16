f7a4840e463f18faa48b1837e0d7fc29778d8a36
Jan Kaiser <jan.kaiser.email@googlemail.com>
2021-05-16 19:31:23 +0200

    Renamed machine class

--- run.py
+++ run.py
@@ -1,14 +1,14 @@
 import signal
 
-from machine import Machine
+from machine import FilmScanner
 
 
 close_requested = False
 
 def cleanup_and_close(sig, frame):
-    machine.close_requested = True
+    scanner.close_requested = True
 
 signal.signal(signal.SIGINT, cleanup_and_close)
 
-machine = Machine()
-machine.run()
+scanner = FilmScanner()
+scanner.run(capture=False)
