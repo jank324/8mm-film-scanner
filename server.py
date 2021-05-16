af9462731c7cac87c7b01731e3d3125648e06f20
Jan Kaiser <jan.kaiser.email@googlemail.com>
2021-05-16 22:25:28 +0200

    Return an image that is actually stored on the Pi

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+import cv2
+from flask import Flask, render_template, Response
 
 
 app = Flask(__name__)
@@ -7,6 +8,15 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/videostream")
+def videostream():
+    image = cv2.imread("test00.jpg", cv2.IMREAD_COLOR)
+    _, jpeg = cv2.imencode(".jpg", image)
+    jpeg_bytes = jpeg.tobytes()
+    packaged = b"--frame\r\n" + b"ContentType: image/jpeg\r\n\r\n" + jpeg_bytes + b"\r\n\r\n"
+    return Response(packaged, mimetype="multipart/x-mixed-replace; boundry=frame")
 
 
 if __name__ == "__main__":
