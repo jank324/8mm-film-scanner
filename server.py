7724f0b4799b47a6afade0015fe3ee28e17eb2c4
Jan Kaiser <jan.kaiser.email@googlemail.com>
2021-05-20 23:07:51 +0200

    Added liveview with smooth frame advance

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 from imutils.video.pivideostream import PiVideoStream
 from flask import Flask, render_template, Response
 
@@ -13,13 +15,22 @@ machine = FilmScanner()
 def index():
     return render_template("index.html")
 
-@app.route("/videostream")
-def videostream():
-    return Response(machine.current_frame(), mimetype="multipart/x-mixed-replace; boundry=frame")
+def gen_liveview():
+    return machine.current_frame()
+    while True:
+        frame = machine.current_frame()
+        yield b"--frame\r\n" + b"ContentType: image/jpeg\r\n\r\n" + frame + b"\r\n\r\n"
+
+@app.route("/liveview")
+def liveview():
+    sleep(2)
+    return Response(gen_liveview(), mimetype="multipart/x-mixed-replace; boundry=frame")
 
 @app.route("/advance", methods=["POST"])
 def advance():
+    machine.camera.pause()
     machine.advance()
+    machine.camera.resume()
     return ("", 204)
 
 
