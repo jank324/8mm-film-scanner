5856c8a2b7e90511be41fd9e3e63e026abe43522
Jan Kaiser <jan.kaiser.email@googlemail.com>
2021-05-20 18:43:53 +0200

    Removed dead code

--- static/script.js
+++ static/script.js
@@ -1,11 +1,9 @@
 $(document).ready(function() {
-    $("#light_button").click(function() {
-        $.post("/light")
-    })
-    
+
     $("#advance_button").click(function() {
         $.post("/advance", function() {
             $("#preview").attr("src", "/videostream")
         })
     })
+    
 })
