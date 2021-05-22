f4fd2e0df837102d1f9a90a539059aa7f9a3008a
Jan Kaiser <jan.kaiser.email@googlemail.com>
2021-05-22 17:41:24 +0200

    Added further controls to focus script

--- focus.py
+++ focus.py
@@ -3,13 +3,12 @@ import numpy as np
 from picamera import PiCamera
 import RPi.GPIO as GPIO
 
-from machine import Light
+from machine import FilmScanner
 
 
 if __name__ == "__main__":
 
-    GPIO.setmode(GPIO.BCM)
-    light = Light(6)
+    scanner = FilmScanner()
 
     with PiCamera() as camera:
         camera.resolution = (1024, 768)
@@ -17,13 +16,13 @@ if __name__ == "__main__":
 
         bgr = np.empty((768,1024,3), dtype=np.uint8)
 
-        while True:
-            camera.capture(bgr, format="bgr", use_video_port=True)
+        for _ in camera.capture_continuous(bgr, format="bgr", use_video_port=True):
             cv2.imshow("Live View", bgr)
 
-            if cv2.waitKey(1) == 27:
+            key = cv2.waitKey(1) & 0xFF
+            if key == ord("q"):
                 break
+            elif key == ord("a"):
+                scanner.advance()
         
         cv2.destroyAllWindows()
-
-    GPIO.cleanup()
