eb2f72fdc963bf550c6ef8f5bec1fe111a729633
Jan Kaiser <jan.kaiser.email@googlemail.com>
2021-05-24 17:14:21 +0200

    Added (not quite functional) histogram

--- newfocus.py
+++ newfocus.py
@@ -2,10 +2,15 @@ import sys
 from threading import Event
 
 import cv2
+import matplotlib
+matplotlib.use("Qt5Agg")
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
+from matplotlib.figure import Figure
 import numpy as np
 from PyQt5.QtCore import pyqtSignal, pyqtSlot, Qt, QThread
 from PyQt5.QtGui import QImage, QPixmap, QPalette, QColor
-from PyQt5.QtWidgets import QWidget, QApplication, QHBoxLayout, QLabel, QPushButton
+from PyQt5.QtWidgets import QWidget, QApplication, QHBoxLayout, QVBoxLayout, QLabel, QPushButton
+from numpy.lib.histograms import histogram
 
 from filmscanner import FilmScanner
 
@@ -20,7 +25,8 @@ def cv2qt(image):
 
 class VideoThread(QThread):
     
-    change_pixmap_signal = pyqtSignal(np.ndarray)
+    change_image_signal = pyqtSignal(np.ndarray)
+    change_histogram_signal = pyqtSignal(np.ndarray)
     not_advancing_event = Event()
 
     def __init__(self, camera):
@@ -33,10 +39,50 @@ class VideoThread(QThread):
 
     def run(self):
         bgr = np.empty((768, 1024,3), dtype=np.uint8)
+        histogram = np.empty((3,256))
 
         for _ in self.camera.capture_continuous(bgr, format="bgr", use_video_port=True):
-            self.change_pixmap_signal.emit(bgr)
+            for i in range(3):
+                histogram[i] = cv2.calcHist([bgr], [i], None, [256], [0,256])[:,0]
+
+            self.change_image_signal.emit(bgr)
+            self.change_histogram_signal.emit(histogram)
+            
             self.not_advancing_event.wait()
+
+
+class Histogram(FigureCanvasQTAgg):
+
+    def __init__(self):
+        self.fig = Figure()
+        self.ax = self.fig.add_subplot(111)
+
+        super().__init__(self.fig)
+
+        self.ax.set_xlim([0, 257])
+        self.ax.set_ylim([0, None])
+        # self.ax.set_xticks([])
+        # self.ax.set_yticks([])
+        
+        self.plot_blue, = self.ax.plot(np.zeros(256), color="b")
+        self.plot_green, = self.ax.plot(np.zeros(256), color="g")
+        self.plot_red, = self.ax.plot(np.zeros(256), color="r")
+
+        # self.fig.tight_layout()
+
+        self.setFixedSize(300, 150)
+    
+    @pyqtSlot(np.ndarray)
+    def update_data(self, histogram):
+        print(histogram.max())
+        self.plot_blue.set_xdata(histogram[0])
+        self.plot_green.set_xdata(histogram[1])
+        self.plot_red.set_xdata(histogram[2])
+
+        # self.ax.set_ylim([0, histogram.max()])
+        self.ax.set_ylim([0, 0.003])
+
+        self.draw()
 
 
 class App(QWidget):
@@ -51,21 +97,31 @@ class App(QWidget):
         self.image_label = QLabel()
         self.image_label.resize(1024, 768)
 
+        self.histogram = Histogram()
+
         self.button = QPushButton("Advance")
         self.button.clicked.connect(self.clicked_advance)
 
+        self.button2 = QPushButton("Other")
+
         hbox = QHBoxLayout()
         hbox.addWidget(self.image_label)
-        hbox.addWidget(self.button)
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.histogram)
+        vbox.addWidget(self.button)
+        vbox.addWidget(self.button2)
+        hbox.addLayout(vbox)
         self.setLayout(hbox)
 
         self.video_thread = VideoThread(self.scanner.camera)
-        self.video_thread.change_pixmap_signal.connect(self.update_image)
+        self.video_thread.change_image_signal.connect(self.update_image)
+        self.video_thread.change_histogram_signal.connect(self.histogram.update_data)
         self.video_thread.start()
     
     @pyqtSlot(np.ndarray)
     def update_image(self, image):
         q_pixmap = cv2qt(image)
+        # print(histogram)
         self.image_label.setPixmap(q_pixmap)
     
     @pyqtSlot()
