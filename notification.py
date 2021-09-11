8da8d4dbb94eabbddeeee3e07d4c8cc6ad594fd9
Jan Kaiser <jan.kaiser.email@googlemail.com>
2021-09-11 16:54:51 +0200

    Moved to new EmailNotifier class

--- notification.py
+++ notification.py
@@ -3,24 +3,33 @@ from email.message import EmailMessage
 import yaml
 
 
-with open("notification_config.yml", "r") as config_file:
-    config = yaml.safe_load(config_file)
+class EmailNotifier:
 
+    def __init__(self):
+        with open("notification_config.yml", "r") as config_file:
+            config = yaml.safe_load(config_file)
+        
+        self._to = config["to"]
+        self._user = config["user"]
+        self._password = config["password"]
 
-def send_notification(text):
-    """Send an e-mail notification to the configured address with `text` as the message."""
-    message = EmailMessage()
-    message.set_content("")
-    message["to"] = config["to"]
-    message["from"] = f"Jan's Film Scanner <{config['user']}>"
-    message["subject"] = text
+        self._from = f"Jan's Film Scanner <{self._user}>"
+    
+    def send(self, text):
+        """Send an e-mail notification to the configured address with `text` as the message."""
+        message = EmailMessage()
+        message.set_content("")
+        message["to"] = self._to
+        message["from"] = self._from
+        message["subject"] = text
 
-    server = smtplib.SMTP("smtp.gmail.com", 587)
-    server.starttls()
-    server.login(config["user"], config["password"])
-    server.send_message(message)
-    server.quit()
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.starttls()
+        server.login(self._user, self._password)
+        server.send_message(message)
+        server.quit()
 
 
 if __name__ == "__main__":
-    send_notification("This is a test!")
+    notifier = EmailNotifier()
+    notifier.send("This is a test!")
