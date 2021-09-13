6df773bfb1a39160c5a5e76953f41bd9c3ce0b54
Jan Kaiser <jan.kaiser.email@googlemail.com>
2021-09-13 22:20:12 +0200

    Started experimenting with a Lightroom plugin for batch flat-field correction

---
+++ flatfield.lrdevplugin/Info.lua
@@ -0,0 +1,12 @@
+return {
+    LrSdkVersion = 10.0,
+    LrToolkitIdentifier = "de.jankaiser.flatfield",
+    LrPluginName = "Flat-Field Batch Helper",
+
+    LrExportMenuItems = {
+        {
+            title = "Run flat-field correction",
+            file = "flatfield.lua"
+        }
+    }
+}
