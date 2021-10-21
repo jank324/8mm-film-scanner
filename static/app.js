e90b154b17e480c967ebbb6a2d6c710e9d83626c
Jan Kaiser <jan.kaiser.email@googlemail.com>
2021-10-21 20:10:57 +0200

    Added fast-forward for starting film

--- static/app.js
+++ static/app.js
@@ -1,7 +1,21 @@
+isFastForwarding = false
+
 $(document).ready(function() {
 
     $("#advance_button").click(function() {
         $.post("/advance")
+    })
+
+    $("#fastforward_button").click(function() {
+        if (!isFastForwarding) {
+            isFastForwarding = true
+            $.post("/fastforward")
+            $("#fastforward_button").html("Stop")
+        } else {
+            isFastForwarding = false
+            $.post("/stop")
+            $("#fastforward_button").html("Fast-Forward")
+        }
     })
 
     $("#focuszoom_button").click(function() {
