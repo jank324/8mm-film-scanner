cf7267628d871f88015b0d0a7bd98e4c7bb97c1c
Jan Kaiser <jan.kaiser.email@googlemail.com>
2022-04-29 23:21:09 +0200

    Fix on load toggle states in backend

--- webserver.py
+++ webserver.py
@@ -22,17 +22,32 @@ scanner.camera.resolution = (800, 600)
 @app.route("/advance", methods=("GET","POST"))
 def advance():
     if request.method == "GET":
-        return Response(advance_trigger_manager.messenger.subscribe(), mimetype="text/event-stream")
+        return {
+            "on": str(scanner.is_advancing and not (scanner.is_fast_forwarding or scanner.is_scanning)),
+            "enabled": not any([
+                scanner.is_advancing,
+                scanner.is_fast_forwarding,
+                scanner.is_scanning
+            ])
+        }
     elif request.method == "POST":
         scanner.advance()
         return "", 204
     return "", 400
 
 
+@app.route("/advance-stream")
+def advance_stream():
+    return Response(advance_trigger_manager.messenger.subscribe(), mimetype="text/event-stream")
+
+
 @app.route("/fastforward", methods=("GET","POST"))
 def fast_forward():
     if request.method == "GET":
-        return Response(fast_forward_manager.messenger.subscribe(), mimetype="text/event-stream")
+        return {
+            "on": str(scanner.is_fast_forwarding),
+            "enabled": not (scanner.is_advancing or scanner.is_scanning)
+        }
     elif request.method == "POST":
         if not scanner.is_fast_forwarding:
             scanner.fast_forward()
@@ -40,6 +55,11 @@ def fast_forward():
             scanner.stop_requested = True
         return "", 204
     return "", 400
+
+
+@app.route("/fastforward-stream")
+def fast_forward_stream():
+    return Response(fast_forward_manager.messenger.subscribe(), mimetype="text/event-stream")
 
 
 @app.route("/preview")
@@ -53,7 +73,7 @@ def liveview():
 @app.route("/light", methods=("GET","POST"))
 def toggle_light():
     if request.method == "GET":
-        return Response(light_toggle_manager.messenger.subscribe(), mimetype="text/event-stream")
+        return {"on": str(scanner.backlight.is_on), "enabled": not scanner.is_scanning}
     elif request.method == "POST":
         if not scanner.is_scanning:
             scanner.backlight.toggle()
@@ -61,14 +81,24 @@ def toggle_light():
     return "", 400
 
 
+@app.route("/light-stream")
+def light_stream():
+    return Response(light_toggle_manager.messenger.subscribe(), mimetype="text/event-stream")
+
+
 @app.route("/focuszoom", methods=("GET","POST"))
 def toggle_focus_zoom():
     if request.method == "GET":
-        return  Response(zoom_toggle_manager.messenger.subscribe(), mimetype="text/event-stream")
+        return {"on": str(scanner.is_zoomed), "enabled": not scanner.is_scanning}
     elif request.method == "POST":
         scanner.live_view_zoom_toggle_requested = True
         return "", 204
     return "", 400
+
+
+@app.route("/focuszoom-stream")
+def focuszoom_stream():
+    return Response(zoom_toggle_manager.messenger.subscribe(), mimetype="text/event-stream")
 
 
 if __name__ == "__main__":
