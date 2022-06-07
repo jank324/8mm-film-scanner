16029f20cba569cc6aa4b2ae2b1a20baf0ffdf4e
Jan Kaiser <jan.kaiser.email@googlemail.com>
2022-06-07 20:53:34 +0200

    Test parallelisation (multiple methods) for dng converter

---
+++ dngconverter_multi.py
@@ -0,0 +1,34 @@
+import argparse
+from concurrent.futures import ProcessPoolExecutor, wait
+import glob
+import os
+
+from pidng.core import RPICAM2DNG
+
+
+def bayerjpg2dng(filepath, delete=False):
+    print(f"Converting {filepath}")
+
+    d = RPICAM2DNG()
+    d.convert(filepath)
+
+    if delete:
+        os.remove(filepath)
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Convert Raspberry Pi Bayer JPEGs to DNGs.")
+    parser.add_argument("directory", help="Directory with the JPEG files")
+    parser.add_argument("--delete", action="store_true")
+    args = parser.parse_args()
+
+    filepaths = glob.glob(f"{args.directory}/frame-*.jpg")
+
+    executor = ProcessPoolExecutor()
+    
+    futures = [executor.submit(bayerjpg2dng, path, delete=args.delete) for path in filepaths]
+    wait(futures)
+
+
+if __name__ == "__main__":
+    main()
