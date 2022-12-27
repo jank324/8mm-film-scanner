f5f3c5a34e6e2cac3136630ef0377c161d4bfadf
Jan Kaiser <jan.kaiser.email@googlemail.com>
2022-12-27 14:51:20 +0100

    Move argument parsing to seperate function

--- dngconverter.py
+++ dngconverter.py
@@ -16,13 +16,21 @@ def bayerjpg2dng(filepath, delete=False):
         os.remove(filepath)
 
 
-def main():
+def parse_arguments() -> argparse.Namespace:
+    """
+    Parse command line arguments to the Pi to DNG conversion utility. Returns the `args`
+    object.
+    """
     parser = argparse.ArgumentParser(
         description="Convert Raspberry Pi Bayer JPEGs to DNGs."
     )
     parser.add_argument("directory", help="Directory with the JPEG files")
     parser.add_argument("--delete", action="store_true")
-    args = parser.parse_args()
+    return parser.parse_args()
+
+
+def main():
+    args = parse_arguments()
 
     filepaths = glob.glob(f"{args.directory}/frame-*.jpg")
 
